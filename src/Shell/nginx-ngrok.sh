b85b95b9906c5dc577095dbc495e66bf2f67cc13
Your Name <you@example.com>
2022-02-08 16:59:41 +0800

    ngrok

--- src/Shell/nginx-ngrok.sh
+++ src/Shell/nginx-ngrok.sh
@@ -4,8 +4,11 @@ echo '开始启动nginx'
 #docker run -d -p 3000:4040  -e NGROK_AUTH=24m3m9usXH4U2KSdbWE7fZTwkvm_3MSyexVzL8dY3VukYkeBp --link mynginx --name myngrok wernight/ngrok ngrok http mynginx:80
 #docker run -d -p 3000:4040   --link mynginx --name myngrok wernight/ngrok ngrok http mynginx:80 #--authtoken 24m3m9usXH4U2KSdbWE7fZTwkvm_3MSyexVzL8dY3VukYkeBp 
 #docker run -e NGROK_AUTHTOKEN=24m3m9usXH4U2KSdbWE7fZTwkvm_3MSyexVzL8dY3VukYkeBp -d -p 3000:4040 ngrok/ngrok http 8080
-docker network create pkngroknet
-docker run -d -p 6379:6379 --net pkngroknet --name myredis redis
-docker run -d -p 4040:4040 --net pkngroknet --name ngrok -e NGROK_PROTOCOL="TCP" -e NGROK_AUTH="24m3m9usXH4U2KSdbWE7fZTwkvm_3MSyexVzL8dY3VukYkeBp" -e NGROK_PORT="myredis:6379" wernight/ngrok 
+
+#docker network create pkngroknet
+#docker run -d -p 6379:6379 --net pkngroknet --name myredis redis
+#docker run -d -p 4040:4040 --net pkngroknet --name ngrok -e NGROK_PROTOCOL="TCP" -e NGROK_AUTH="24m3m9usXH4U2KSdbWE7fZTwkvm_3MSyexVzL8dY3VukYkeBp" -e NGROK_PORT="myredis:6379" wernight/ngrok
+
+docker run -d -p 4040:4040 --net pkngroknet --name ngrok -e NGROK_PROTOCOL="TCP" -e NGROK_AUTH="24m3m9usXH4U2KSdbWE7fZTwkvm_3MSyexVzL8dY3VukYkeBp" -e NGROK_PORT="redis-node1:6379" wernight/ngrok 
 echo '启动完成'
 
