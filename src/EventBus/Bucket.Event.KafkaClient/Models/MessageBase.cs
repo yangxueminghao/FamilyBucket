95465af96f1a5194f9c1f07c47cdee5c15a68232
Joe <yangxueminghao@163.com>
2022-08-26 09:38:11 +0800

    Message

---
+++ src/EventBus/Bucket.Event.KafkaClient/Models/MessageBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bucket.Event.KafkaClient.Models
+{
+    public class MessageBase
+    {
+        public MessageBase()
+        {
+            CreateTime = DateTimeOffset.Now;
+            MsgNo = Guid.NewGuid().ToString();
+        }
+
+        /// <summary>
+        /// 业务类型
+        /// </summary>
+        public int BusType { get; set; }
+        /// <summary>
+        /// 生成时间
+        /// </summary>
+        public DateTimeOffset CreateTime { get; set; }
+        /// <summary>
+        /// 消息编号
+        /// </summary>
+        public string MsgNo { get; set; }
+        /// <summary>
+        /// 客户端名称
+        /// </summary>
+        public string ClientName { get; set; }
+
+    }
+}
