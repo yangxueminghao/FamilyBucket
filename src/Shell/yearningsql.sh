693503e9aff8479fc125c4b477b95565fc69cc52
Joe <yangxueminghao@163.com>
2022-09-22 15:57:44 +0800

    yearning

--- src/Shell/yearningsql.sh
+++ src/Shell/yearningsql.sh
@@ -6,5 +6,8 @@ git clone https://github.com/cookieY/Yearning.git ##没有git的需自行安装 
 cd Yearning/install/yearning-docker-compose
 docker-compose up -d
 #docker run -d --link=some-mysql -it -p8000:8000 -e MYSQL_USER=root -e MYSQL_ADDR=some-mysql:3306 -e MYSQL_PASSWORD=123456 -e MYSQL_DB=yearning   zhangsean/yearning
+#docker exec -it 102 /bin/sh
+#CMD ["/bin/sh"] ./Yearning install
+
 echo '启动完成'
 
